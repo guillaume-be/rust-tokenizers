c6c7ccf89c45c5386bfb6302db8aebd702b5f269
Guillaume B <guillaume.becquin@gmail.com>
2020-05-23 12:20:59 +0200

    Updated build scripts

--- main/build.rs
+++ main/build.rs
@@ -1,22 +1,20 @@
-#[cfg(feature = "proto-compile")]
 use std::fs;
 
 fn main() {
-    #[cfg(feature = "proto-compile")]
-        {
-            let out_path = "src/preprocessing/vocab/sentencepiece_proto";
-            let out_file_name = "src/preprocessing/vocab/sentencepiece_proto/sentencepiece_model.proto";
-            let proto_path = "protos/sentencepiece_model.proto";
+    {
+        let out_path = "src/preprocessing/vocab/sentencepiece_proto";
+        let out_file_name = "src/preprocessing/vocab/sentencepiece_proto/sentencepiece_model.proto";
+        let proto_path = "protos/sentencepiece_model.proto";
 
-            let metadata = fs::metadata(out_file_name);
+        let metadata = fs::metadata(out_file_name);
 
-            if !(metadata.is_ok()) {
-                protobuf_codegen_pure::Codegen::new()
-                    .out_dir(out_path)
-                    .inputs(&[proto_path])
-                    .include("protos")
-                    .run()
-                    .expect("Codegen failed.");
-            }
+        if !(metadata.is_ok()) {
+            protobuf_codegen_pure::Codegen::new()
+                .out_dir(out_path)
+                .inputs(&[proto_path])
+                .include("protos")
+                .run()
+                .expect("Codegen failed.");
         }
+    }
 }
