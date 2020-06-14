e4d053b44b21dfd91b215859c8942462363394c3
Guillaume B <guillaume.becquin@gmail.com>
2020-06-14 15:11:45 +0200

    Addition of Python bindings for ALBert

--- python-bindings/rust_tokenizers/__init__.py
+++ python-bindings/rust_tokenizers/__init__.py
@@ -1,3 +1,3 @@
-from .rust_tokenizers import PyBertTokenizer, PyCtrlTokenizer, PyGpt2Tokenizer, PyRobertaTokenizer, PyOpenAiGptTokenizer, PySentencePieceTokenizer
+from .rust_tokenizers import PyBertTokenizer, PyCtrlTokenizer, PyGpt2Tokenizer, PyRobertaTokenizer, PyOpenAiGptTokenizer, PySentencePieceTokenizer, PyAlbertTokenizer
 
-__all__ = ["PyBertTokenizer", "PyCtrlTokenizer", "PyGpt2Tokenizer", "PyRobertaTokenizer", "PyOpenAiGptTokenizer", "PySentencePieceTokenizer"]
+__all__ = ["PyBertTokenizer", "PyCtrlTokenizer", "PyGpt2Tokenizer", "PyRobertaTokenizer", "PyOpenAiGptTokenizer", "PySentencePieceTokenizer", "PyAlbertTokenizer"]
