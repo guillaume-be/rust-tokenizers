49b66820b1d7af22930f6b02f3d7953b637a4a59
guillaume-be <guillaume.becquin@gmail.com>
2020-10-09 16:09:08 +0200

    top-level documentation

--- main/src/lib.rs
+++ main/src/lib.rs
@@ -10,6 +10,48 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//!# High performance tokenizers for Rust
+//!
+//! This crate contains implementation of common tokenizers used in state-of-the-art language models.
+//! It is usd as the reference tokenization crate of [rust-bert](https://docs.rs/rust-bert/), exposing modern transformer-based
+//! models such as BERT, RoBERTa, GPT2, BART, XLNet...
+//!
+//! The following tokenizers have been implemented and validated against a Python reference implementation:
+//! - Sentence Piece (unigram model)
+//! - BERT
+//! - DistilBERT
+//! - RoBERTa
+//! - GPT
+//! - GPT2
+//! - CTRL
+//! - XLNet
+//!
+//! The library is structured into vocabularies (for the encoding and decoding of the tokens and registration of special tokens)
+//! and tokenizers (splitting the input text into tokens). Generally, a tokenizer will contain a reference vocabulary that may
+//! be used as part of the tokenization process (for example, containing a list of subwords or merges).
+//!
+//! ## Usage example
+//!
+//! ```no_run
+//! use std::sync::Arc;
+//! use rust_tokenizers::{Vocab, BertTokenizer, Tokenizer, TruncationStrategy};
+//! use rust_tokenizers::preprocessing::adapters::Example;
+//! # fn main() -> anyhow::Result<()> {
+//! let vocab_path = "path/to/vocab";
+//! let vocab = Arc::new(rust_tokenizers::BertVocab::from_file(&vocab_path)?);
+//!
+//! let test_sentence = Example::new_from_string("This is a sample sentence to be tokenized");
+//! let bert_tokenizer: BertTokenizer = BertTokenizer::from_existing_vocab(vocab.clone(), true, true);
+//!
+//! println!("{:?}", bert_tokenizer.encode(&test_sentence.sentence_1,
+//!                                        None,
+//!                                        128,
+//!                                        &TruncationStrategy::LongestFirst,
+//!                                        0));
+//! # Ok(())
+//! # }
+//! ```
+
 pub mod preprocessing;
 
 pub use crate::preprocessing::error;
